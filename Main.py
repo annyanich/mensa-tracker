108c6e12e3477797901f54dd17f3671b0a415966
Ann Yanich <ann.yanich@gmail.com>
2016-05-14 22:59:34 +0200

    Clean up repository
    Delete unused/unnecessary files (Main.py, urls.py).
    Reorganize test menu pages: Delete old ones and download new ones,
    plus their embedded images, so that they will load quickly in a
    browser rather than hang around waiting for files that aren't there.

--- Main.py
+++
@@ -1,8 +0,0 @@
-# I'm going to just doodle around in here and see what I can get working.
-from bs4 import BeautifulSoup
-
-testFilename = "test menu pages/uhlhornsweg-ausgabe-a.php"
-testFile = open(testFilename)
-soup = BeautifulSoup(testFile, 'html.parser', from_encoding="utf-8")
-
-print(soup.prettify())
