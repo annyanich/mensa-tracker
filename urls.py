67f9a1c29ff0484c135c8ac60a84dcdbd84419ff
Ann Yanich <ann.yanich@gmail.com>
2016-05-15 17:06:06 +0200

    Delete urls.py (unused file)

--- urls.py
+++
@@ -1,3 +0,0 @@
-ausgabeA = "http://www.studentenwerk-oldenburg.de/speiseplan/uhlhornsweg-ausgabe-a.php"
-ausgabeB = "http://www.studentenwerk-oldenburg.de/speiseplan/uhlhornsweg-ausgabe-b.php"
-culinarium = "http://www.studentenwerk-oldenburg.de/speiseplan/culinarium.php"
