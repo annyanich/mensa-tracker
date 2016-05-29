2acff41b2ac4082ae5effa807bbeb8dd51ecac0b
Ann Yanich <ann.yanich@gmail.com>
2016-05-29 18:16:44 +0200

    Remove Apache2 from Vagrant image

--- Vagrant-bootstrap.sh
+++ Vagrant-bootstrap.sh
@@ -2,12 +2,6 @@
 
 apt-get update
 
-apt-get install -y apache2
-if ! [ -L /var/www ]; then
-  rm -rf /var/www
-  ln -fs /vagrant /var/www
-fi
-
 apt-get install -y python3-pip python3-dev libffi-dev libssl-dev libxml2-dev libxslt1-dev python3-lxml
 /usr/bin/pip3 install -r /vagrant/requirements.txt
 python3 /vagrant/oauth_example_app.py db init
