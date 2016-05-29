8773819fa7b8b7bb48a1a582611065c442f10bf1
Ann Yanich <ann.yanich@gmail.com>
2016-05-29 18:42:20 +0200

    Refactoring (part 2) Move config settings out of app/__init__.py into a separate file (settings.py) Move database migrations from app/migrations into /db_repository

--- app/__init__.py
+++ app/__init__.py
@@ -17,23 +17,7 @@ from oauth import OAuthSignIn, FacebookSignIn
 
 app = Flask(__name__)
 
-app.config['DEBUG'] = None
-# SECRET_KEY is just some random secret key that you need to make.  IDK why
-app.config['SECRET_KEY'] = 'V\x8cjc\xff\xb8\x02\x9f@JV\r\xd9K\xe9\xd5\xe0\xa1m\x9e\xd0 \x99*'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(
-    os.path.dirname(os.path.realpath(__file__)),
-    'mensa-scraper-oauth-test-db.sqlite'
-)
-app.config['OAUTH_CREDENTIALS'] = {
-    'facebook': {
-        'id': '215569478828174',
-        'secret': '4ed42c146ac79988bb7073dd77514a45'
-    },
-    # 'twitter': {
-    #     'id': '3RzWQclolxWZIMq5LJqzRZPTl',
-    #     'secret': 'm9TEd58DSEtRrZHpz2EjrV9AhsBRxKMo8m3kuIZj3zLwzwIimt'
-    # }
-}
+app.config.from_object('config')
 
 # Expose datetime-formatting functions to all templates
 from momentjs import Momentjs
---
+++ config.py
@@ -0,0 +1,25 @@
+# From Miguel Grinberg's Flask tutorial
+# Activates cross-site request forgery prevention for the Flask-WTF extension
+# WTF_CSRF_ENABLED = True
+
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db.sqlite')
+SQLALCHEMY_DB_REPO = os.path.join(basedir, 'db_repository')
+
+DEBUG = None
+# SECRET_KEY is just some random secret key that you need to make.  IDK why
+SECRET_KEY =  'V\x8cjc\xff\xb8\x02\x9f@JV\r\xd9K\xe9\xd5\xe0\xa1m\x9e\xd0 \x99*'
+
+OAUTH_CREDENTIALS = {
+    'facebook': {
+        'id': '215569478828174',
+        'secret': '4ed42c146ac79988bb7073dd77514a45'
+    },
+    # 'twitter': {
+    #     'id': '3RzWQclolxWZIMq5LJqzRZPTl',
+    #     'secret': 'm9TEd58DSEtRrZHpz2EjrV9AhsBRxKMo8m3kuIZj3zLwzwIimt'
+    # }
+}
