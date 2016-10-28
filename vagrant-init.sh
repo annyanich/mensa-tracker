ea5e627864edfdeb2e5c12450429eaa0ce3bf2ee
Ann Yanich <ann.yanich@gmail.com>
2016-10-28 20:48:57 +0200

    Install Heroku inside of Vagrant VM
    heroku-install-ubuntu.sh:
    - Installation script provided by Heroku.
    
    vagrant-init.sh:
    - Add Ubuntu's libpq-dev package, which is needed to install psycopg2
    via pip.
    - Run Heroku installation script.
    - Run db initialization commands through Heroku (via 'heroku local init'
    and 'heroku local upgrade'), rather than calling them directly. The
    corresponding shell commands are now specified in Heroku's Procfile.

--- vagrant-init.sh
+++ vagrant-init.sh
@@ -4,8 +4,13 @@ apt-get update
 
 apt-get install -y python3-pip python3-dev libffi-dev libssl-dev libxml2-dev libxslt1-dev python3-lxml
 apt-get install -y ipython3 # For convenience during development
+apt-get install -y libpq-dev # Needed by SQLAlchemy to connect to PostgreSQL
+
 /usr/bin/pip3 install -r /vagrant/requirements.txt
 
+# Run the Heroku installation script
+sh /vagrant/heroku-install-ubuntu.sh
+
 # Initialize our db and upgrade it to the latest schema
-python3 /vagrant/run.py db init
-cd /vagrant && python3 run.py db upgrade
+cd /vagrant && heroku local init
+cd /vagrant && heroku local upgrade
