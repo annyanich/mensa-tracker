52f663c4f4dda22ece1364585067c1cdad796296
Ann Yanich <ann.yanich@gmail.com>
2016-10-28 22:45:02 +0200

    Set a logger on Flask app so errors will get logged in Heroku

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@
 #
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+import logging
+import sys
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -13,3 +15,6 @@ from app import views, models
 # Expose datetime-formatting functions to all templates
 from app.momentjs import Momentjs
 app.jinja_env.globals['Momentjs'] = Momentjs
+
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
