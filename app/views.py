6b9deca67572638f1c12604da260fcee89153c9e
Ann Yanich <ann.yanich@gmail.com>
2016-11-28 00:07:34 +0100

    Revise website
    index.html: Revise navbar.  Minor tweaks to wording.
    
    mensahistory.html: Revise navbar.
    Remove German explanatory text.  Replace it with simpler English text.
    
    about.html: Briefly explain what this website is about.
    
    views.py: Change default view ('/') to point to the interactive saved
    searches page.
    Move menu data table to '/mensa-history'.
    Add route '/about'

--- app/views.py
+++ app/views.py
@@ -14,14 +14,18 @@ import re
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-@app.route('/')
+@app.route('/mensa-history')
 def mensa_history():
     menu_entries = MenuEntry.query.order_by(MenuEntry.date_valid)
     return render_template('mensahistory.html', menu_entries=menu_entries)
 
-@app.route('/index.html')
+@app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 @app.route('/logout')
 def logout():
