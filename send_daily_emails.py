65ffdedd4336e7e7c168c71617ec14e48dc0cb31
Ann Yanich <ann.yanich@gmail.com>
2016-12-02 17:59:41 +0100

    Send alerts for today's menu entries instead of tomorrow's
    My reasoning for this change is as follows:
    
    1. In my current configuration on Heroku,
    I'm queuing and sending the emails after midnight. This means that right
    now, I'm sending out all the emails one day too early. I wake up on
    Thursday and find the email alerts for Friday in my inbox. It's dumb.
    
    2. Upon reflection, I don't think it makes sense to send out alerts the
    night before. I think relatively few people will check their email at
    night and make a note to themselves to go to the mensa the next day.
    
    email_alerter.py: Remove all references to tomorrow. Use today's date
    instead.  Rename functions and variables accordingly.
    
    send_daily_emails.py:
    "run_all_searches_for_tomorrow()" becomes "run_all_searches_for_today()".

--- send_daily_emails.py
+++ send_daily_emails.py
@@ -2,10 +2,10 @@
 # It runs all of the searches in the database against the menu for tomorrow,
 # then it queues up an email to each user who has at least one search result.
 
-from email_alerter.email_alerter import search_hits_to_emails, run_all_searches_for_tomorrow
+from email_alerter.email_alerter import search_hits_to_emails, run_all_searches_for_today
 from email_alerter import celery_tasks
 
 if __name__ == '__main__':
-    for recipient, subject, body in search_hits_to_emails(run_all_searches_for_tomorrow()):
+    for recipient, subject, body in search_hits_to_emails(run_all_searches_for_today()):
         celery_tasks.send_email.delay(recipient, subject, body)
         # TODO log the emails we send out?
