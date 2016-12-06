7a0d40fef165838f85f26dc5a23a1d9eaf3b7bc0
Ann Yanich <ann.yanich@gmail.com>
2016-12-06 01:40:14 +0100

    Revert "Add script to run 'heroku local web' as a PyCharm run configuration"
    This reverts commit acb35a3c77d47a4bb319630b57ecc7e6120fa315.
    The script is not necessary.  In INSTALL.md I explain how to do your run
    configurations in a better way.

--- run-heroku-local-web.py
+++
@@ -1,7 +0,0 @@
-#!/bin/python3
-#
-# This script is used in my PyCharm run configuration for the Flask web server.
-# I wasn't sure how else to run the 'heroku' command inside of the Vagrant VM from PyCharm.
-
-import os
-os.system('heroku local web')
