785e2e19f0d49553c01b84f13e1043d90d327d0a
Ann Yanich <ann.yanich@gmail.com>
2016-12-07 01:58:38 +0100

    Fix Vagrant initialization script (bootstrap.sh)
    Stop using the installation script (heroku-install-ubuntu.sh) provided
    by Heroku. Instead, we just import their public key
    (heroku_release.key), add their repo to apt, and install the
    heroku-toolkit package ourselves in bootstrap.sh.
    
    Also remove a command that doesn't work unattended (heroku local
    upgrade) because the Heroku CLI always prompts for credentials the first
    time you run it.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,24 +1,25 @@
 #!/usr/bin/env bash
 
+# add heroku repository to apt
+echo "deb http://toolbelt.heroku.com/ubuntu ./" > /etc/apt/sources.list.d/heroku.list
+
+# install heroku's release key for package verification
+apt-key add /vagrant/heroku_release.key
+
 apt-get update
 
 apt-get install -y python3-pip python3-dev libffi-dev libssl-dev libxml2-dev libxslt1-dev python3-lxml
-apt-get install -y ipython3 # For convenience during development
+apt-get install -y git ipython3 # For convenience during development
 apt-get install -y libpq-dev # Needed by SQLAlchemy to connect to PostgreSQL
 apt-get install -y postgresql
 apt-get install -y rabbitmq-server
+apt-get install -y heroku-toolbelt
 
 /usr/bin/pip3 install -r /vagrant/requirements.txt
-
-sh /vagrant/heroku-install-ubuntu.sh
 
 sudo -u postgres createuser --superuser vagrant
 sudo -u postgres createdb vagrant
 sudo -u postgres psql -c "ALTER USER vagrant WITH PASSWORD 'password';"
-
-# Initialize our db and upgrade it to the latest schema
-cd /vagrant && heroku local init
-cd /vagrant && heroku local upgrade
 
 # Set up RabbitMQ (used to queue up emails)
 rabbitmqctl add_user devuser test123 #username password
