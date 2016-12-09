2a06db970402928284f345bb13a6f44dd82a1807
Ann Yanich <ann.yanich@gmail.com>
2016-12-09 20:56:47 +0100

    Print messages instead of raising ValueError when config vars are missing
    Also make minor revisions to the warning messages.
    
    We should really use the 'logger' module instead of just 'print()' for
    our logging. This is a first step.

--- config.py
+++ config.py
@@ -9,13 +9,17 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # Heroku gives us the url of the app's Postgres database via the environment
 # variable 'DATABASE_URL'.
 if os.environ.get('DATABASE_URL') is None:
-    raise ValueError('The environment variable "DATABASE_URL" is not specified.  '
-                     'Please specify a database url for SQLAlchemy to connect to.')
+    raise ValueError('The environment variable "DATABASE_URL" is missing.  '
+                     'It should point to your SQL database.  '
+                     'If you see this error while deployed to Heroku, you '
+                     'probably need to add the heroku-postgresql addon to '
+                     'your app.')
 else:
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
-# Explicitly disable Flask-SQLAlchemy's event notification system.  It defaults to False,
-# but Flask will throw a warning unless we explicitly set it to False ourselves.
+# Explicitly disable Flask-SQLAlchemy's event notification system.  It
+# defaults to False, but Flask will throw a warning unless we explicitly set
+# it to False ourselves.
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 if os.environ.get('DEBUG') is None:
@@ -36,44 +40,47 @@ else:
 
 # SECRET_KEY is a cryptographic secret used by Flask.
 if os.environ.get('SECRET_KEY') is None:
-    raise ValueError('The environment variable "SECRET_KEY" is missing.  '
-                     'Please specify a SECRET_KEY for Flask to use as a cryptographic secret.'
-                     'See http://flask.pocoo.org/docs/0.11/quickstart/ for an example of how to generate one.')
+    print('The environment variable "SECRET_KEY" is missing.  '
+          "Without it, Flask won't work.  "
+          'See http://flask.pocoo.org/docs/0.11/quickstart/ for an example '
+          'of how to generate one.')
 else:
     SECRET_KEY = os.environ['SECRET_KEY']
 
 OAUTH_CREDENTIALS = {'facebook': {}}
 
+
 if os.environ.get('FACEBOOK_APP_ID') is None:
-    raise ValueError('The environment variable "FACEBOOK_APP_ID" is missing.'
-                     'Please specify it to allow users to login with Facebook.'
-                     'You can get this from developers.facebook.com after creating an app there.')
+    print('The environment variable "FACEBOOK_APP_ID" is missing.  '
+          "Without it, users won't be able to log in to our website.  "
+          "You can get it from your app's control panel in "
+          'developers.facebook.com.')
 else:
     OAUTH_CREDENTIALS['facebook']['id'] = os.environ['FACEBOOK_APP_ID']
 
 if os.environ.get('FACEBOOK_APP_SECRET') is None:
-    raise ValueError('The environment variable "FACEBOOK_APP_SECRET" is missing.'
-                     'Please specify it to allow users to login with Facebook.'
-                     'You can get this from developers.facebook.com after creating an app there.')
+    print('The environment variable "FACEBOOK_APP_SECRET" is missing.  '
+          "Without it, users won't be able to log in to our website.  "
+          "You can get it from your app's control panel in "
+          'developers.facebook.com.')
 else:
     OAUTH_CREDENTIALS['facebook']['secret'] = os.environ['FACEBOOK_APP_SECRET']
 
 if os.environ.get('RABBITMQ_BIGWIG_URL') is None:
-    raise ValueError("The environment variable 'RABBITMQ_BIGWIG_URL' is missing.  "
-                     "Please specify it to allow email alerts to be queued up in RabbitMQ.  "
-                     "This is normally provided by the RabbitMQ Bigwig addon in Heroku.")
+    print("The environment variable 'RABBITMQ_BIGWIG_URL' is missing.  "
+          "Without it, we can't queue up emails in RabbitMQ.  "
+          "This is normally provided by the RabbitMQ Bigwig addon in Heroku.")
 else:
     RABBITMQ_BIGWIG_URL = os.environ['RABBITMQ_BIGWIG_URL']
 
 if os.environ.get('GMAIL_USERNAME') is None:
-    raise ValueError("The environment variable 'GMAIL_USERNAME' is missing.  "
-                     "Please provide it so email alerts can be sent out with yagmail.")
+    print("The environment variable 'GMAIL_USERNAME' is missing.  "
+          "Without it, we can't send out email alerts.")
 else:
     GMAIL_USERNAME = os.environ['GMAIL_USERNAME']
 
 if os.environ.get('GMAIL_PASSWORD') is None:
-    raise ValueError("The environment variable 'GMAIL_PASSWORD' is missing.  "
-                     "Please provide it so email alerts can be sent out with "
-                     "yagmail.")
+    print("The environment variable 'GMAIL_PASSWORD' is missing.  "
+          "Without it, we can't send out email alerts.")
 else:
     GMAIL_PASSWORD = os.environ['GMAIL_PASSWORD']
