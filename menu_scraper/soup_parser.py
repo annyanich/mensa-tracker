95753621119cd056f99b553585b08520faea48ee
Ann Yanich <ann.yanich@gmail.com>
2016-12-25 02:56:28 +0100

    Keep renaming stuff and reformatting code in soup_parser.py

--- menu_scraper/soup_parser.py
+++ menu_scraper/soup_parser.py
@@ -23,21 +23,22 @@ def get_all_menu_items(response):
 
 
 def get_menu_tables(soup):
-    # The summary attribute for the table with the menu plan says something
-    # like "Wochenplan Uweg Ausgabe B (aktuelle Woche)".
+    """
+    The 'summary' attribute should be something like
+    "Wochenplan Uweg Ausgabe B (aktuelle Woche)".
+    """
     def is_menu_table(tag):
         return tag.name == 'table' and tag.has_attr('summary')\
             and 'Wochenplan' in tag['summary']
+
     return soup.findAll(is_menu_table)
 
 
 def parse_table(menu_table):
     """
-    Parse the name, price, allergens, category, etc. of every menu entry
-    in a table.
+    Extract the name, price, allergens, etc. of every menu item in a table.
     :param menu_table: An HTML table object extracted by BeautifulSoup.
-    :return A list of MenuEntry Items describing all the dishes offered in
-    the menu.
+    :return A list of MenuEntry Items describing all the dishes on the menu.
     """
     # The date range of the table is posted above it.
     date_range = get_date_range_from_string(menu_table.previous_sibling)
@@ -49,14 +50,14 @@ def parse_table(menu_table):
         entries_monday_to_friday = get_menu_entries_for_five_days(category_row)
 
         for td, date in zip(entries_monday_to_friday, date_range):
-            menu_item_divs = td.find_all(attrs={'class': 'speise_eintrag'})
+            menu_item_elements = td.find_all(attrs={'class': 'speise_eintrag'})
 
-            for menu_item_div in menu_item_divs:
-                item = item_from_string(menu_item_div.text)
+            for element in menu_item_elements:
+                item = item_from_string(element.text)
                 item['category'] = get_category_name_from_row(category_row)
                 item['date_valid'] = date
                 if not item['price']:
-                    item['price'] = get_price_of_category(category_row)
+                    item['price'] = get_price_from_row(category_row)
                 yield item
 
 
@@ -67,13 +68,13 @@ def get_date_range_from_string(date_range_string):
     :return A list of datetime.date objects representing the dates the menu
     is valid for.
     """
-    regex = '[0-9][0-9]\.[0-9][0-9]\.[0-9][0-9]'
-    date_strings = re.findall(regex, date_range_string)
+    date_regex = '[0-9][0-9]\.[0-9][0-9]\.[0-9][0-9]'
+    date_strings = re.findall(date_regex, date_range_string)
     assert len(date_strings) == 2, (
         'The date range string found above a menu does not contain exactly '
         'two dates as determined by our Regex. \n'
         'Date string: {0}\nRegex: {1}'.format(date_range_string,
-                                              regex))
+                                              date_regex))
 
     # Convert from strings into datetime.date values
     dates = [datetime.datetime.strptime(string, "%d.%m.%y").date()
@@ -86,60 +87,64 @@ def get_date_range_from_string(date_range_string):
 
     range_length = (end_date - start_date).days + 1
 
-    assert range_length == 5, \
-        ("We assume that each menu covers five days, but this menu has the "
-         "following dates posted above it: \n{0}\n "
-         "Maybe our assumption is no longer true.".format(date_range_string))
+    assert range_length == 5, (
+        "We assume that each menu covers five days, but this menu has the "
+        "following dates posted above it: \n{0}\n "
+        "Maybe our assumption is no longer true.".format(date_range_string))
 
     date_range = [(start_date + datetime.timedelta(days=n))
                   for n in range(range_length)]
     return date_range
 
 
-def get_menu_entries_for_five_days(row_of_menu):
-    cells = row_of_menu.find_all('td', recursive=False)
+def get_menu_entries_for_five_days(category_row):
+    cells = category_row.find_all('td', recursive=False)
     assert len(cells) == 6, (
         "A row in the Speisekarte should be six columns wide: One category "
         "name plus five days' worth of menu entries. This row is either too "
         "wide or not wide enough: \n" +
         "\n".join("Column {i}: {text}".format(i=i, text=td.text)
                   for i, td in enumerate(cells)))
+
     return cells[1:]
 
 
-def get_price_of_category(row):
-    first_cell_in_row = row.find('td', recursive=False)
+def get_price_from_row(category_row):
+    """
+    :return: The price (in cents), if any, listed in the first cell of the row.
+    If there is no price listed, return None.
+    """
+    first_cell_in_row = category_row.find('td', recursive=False)
     return get_price_from_string(first_cell_in_row.text)
 
 
-def get_category_name_from_row(row):
+def get_category_name_from_row(category_row):
     """
     Clean up e.g. "Haupt-\ngericht", "Beilagen\n 0,35", "Veggie/Vegan"
     to give us "Hauptgericht", "Beilagen", "Veggie/Vegan"
     """
-    first_cell_in_row = row.find('td', recursive=False)
+    first_cell_in_row = category_row.find('td', recursive=False)
     return re.sub('[^a-zA-ZäüößÄÜÖ/]', '', first_cell_in_row.text)
 
 
 def item_from_string(item_string):
     """
     Extract the price (if listed), name, and allergens of a menu item.
-    :return: A MenuEntry Item object used by Scrapy
+    :param item_string: Something like 'Spaghetti (Gl)' or 'Eisbergsalat 0,35'
+    :return: A MenuEntry Item object usable by Scrapy
     """
-    # Each item has its allergens listed after it in parentheses,
-    # e.g. 'Spaghetti (Gl)', 'Eisbergsalat 0,35'
-    # We want to split these into their components:
-    # item_split=['Spaghetti', '(Gl)', ''] or ['Eisbergsalat 0,35']
+    # Split the item_string into its components, e.g.
+    # ['Spaghetti', '(Gl)', ''] or ['Eisbergsalat 0,35']
     item_split = re.split("(\(.+?\))", item_string.strip())
     assert len(item_split) == 1 or len(item_split) == 3
 
     allergens = item_split[1] if len(item_split) == 3 else ""
 
-    item_name = fix_garbled_german_characters(item_split[0])
-    item_name = remove_price_from_string(item_name)
-    item_name = clean_up_whitespace(item_name)
-
     price = get_price_from_string(item_string)
+
+    item_name = fix_garbled_german_characters(item_split[0])
+    item_name = strip_price_from_string(item_name)
+    item_name = clean_up_whitespace(item_name)
 
     return menu_scraper.items.MenuEntry(
         price=price,
@@ -149,12 +154,7 @@ def item_from_string(item_string):
 
 
 def fix_garbled_german_characters(string):
-    """
-    This is necessary due to some kind of encoding issue that I have been
-    unable to resolve.
-    :param string: A string that may contain Umlauts or Eszets.
-    :return: The same string, with possibly garbled characters fixed.
-    """
+    """This is a workaround for a weird encoding issue I ran into."""
     return string.replace('Ã¼', 'ü')\
                  .replace('Ã¶', 'ö')\
                  .replace('Ã¤', 'ä')\
@@ -172,7 +172,7 @@ def clean_up_whitespace(string):
     return ' '.join(string_without_line_wraps.split())
 
 
-def remove_price_from_string(string):
+def strip_price_from_string(string):
     return re.sub("[0-9],[0-9][0-9]", "", string)
 
 
