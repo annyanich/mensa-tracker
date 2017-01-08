51b5a03944272dd57f53a085738dd898855804fd
Ann Yanich <ann.yanich@gmail.com>
2017-01-08 18:06:06 +0100

    Test scraping the 2017-formatted menu pages for wechloy and ofener strasse

--- config.py
+++ config.py
@@ -110,7 +110,9 @@ menu_urls_and_names = {
     "file:///vagrant/test%20menu%20pages/uhlhornsweg-classic-20170106.html":
         "Uhlhornsweg Classic",
     # "file:///vagrant/test%20menu%20pages/wechloy-20161222.html": "Wechloy",
+    "file:///vagrant/test%20menu%20pages/wechloy-20170106.html": "Wechloy",
     # "file:///vagrant/test%20menu%20pages/ofener-strasse-20161223.html": "Ofener Straße",
+    "file:///vagrant/test%20menu%20pages/ofener-strasse-20170106.html": "Ofener Straße",
     # "file:///vagrant/test%20menu%20pages/emden-20161223.html": "Emden",
     # "file:///vagrant/test%20menu%20pages/wilhelmshaven-20161223.html": "Wilhelmshaven",
     # "file:///vagrant/test%20menu%20pages/elsfleth-20161223.html": "Elsfleth"
