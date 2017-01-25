ba1ad8e5ebfc7ac754133865e505bdc1c7ca9680
Ann Yanich <ann.yanich@gmail.com>
2017-01-25 04:53:10 +0100

    Improve formatting of email alerts (including price information) & enable generating email alerts for any given date

--- email_alerter/email_alerter.py
+++ email_alerter/email_alerter.py
@@ -1,3 +1,5 @@
+from locale import currency
+
 from flask_app.models import SavedSearch, MenuEntry, User
 
 import datetime
@@ -14,15 +16,19 @@ Ann Yanich (http://mensa-tracker.herokuapp.com)
 EMAIL_SUBJECT_TEMPLATE = "Mensa tracker alert for {date}"
 
 
-def test_email_generation():
+def test_email_generation(date):
     # TODO convert this into a unit test
-    for recipient, subject, body in email_alerts_for_today():
+    for recipient, subject, body in email_alerts_for_date(date):
         print("Recipient: {0}\n"
               "Subject: {1}\n"
               "Body: {2}\n".format(recipient, subject, body))
 
 
 def email_alerts_for_today():
+    return email_alerts_for_date(datetime.date.today())
+
+
+def email_alerts_for_date(date):
     """
     For each user, run their saved searches.  Write an email to them if they have
     search results.
@@ -31,13 +37,13 @@ def email_alerts_for_today():
     each tuple represents one email to be sent out.
     """
 
-    email_subject = EMAIL_SUBJECT_TEMPLATE.format(date=datetime.date.today().strftime("%A, %d.%m.%Y"))
+    email_subject = EMAIL_SUBJECT_TEMPLATE.format(date=date.strftime("%A, %d.%m.%Y"))
 
     for user in User.query.all():
         formatted_search_results = ""
 
         for search in SavedSearch.query.filter_by(owner=user).all():
-            search_results = [menu_entry for menu_entry in menu_entries_today()
+            search_results = [menu_entry for menu_entry in menu_entries_for_date(date)
                               if does_search_match(search.search_terms, menu_entry.description)]
 
             if search_results:
@@ -46,7 +52,7 @@ def email_alerts_for_today():
                     formatted_search_results += menu_entry_to_text(menu_entry) + "\n\n"
 
         if formatted_search_results == "":
-            continue;  # Do not send an email if there are no search results.
+            continue  # Do not send an email if there are no search results.
 
         # Fill out the email body template with the information we have gathered
         email_body = EMAIL_BODY_TEMPLATE.format(name=user.nickname,
@@ -55,8 +61,8 @@ def email_alerts_for_today():
         yield (user.email, email_subject, email_body)
 
 
-def menu_entries_today():
-    return MenuEntry.query.filter_by(date_valid=datetime.date.today()).all()
+def menu_entries_for_date(date):
+    return MenuEntry.query.filter_by(date_valid=date).all()
 
 
 def does_search_match(search_terms, body):
@@ -67,10 +73,17 @@ def does_search_match(search_terms, body):
 
 def menu_entry_to_text(entry):
     return ("{date_valid}\n"
-            "{mensa}: {category}\n"
-            "{description}").format(
+            "{mensa}\n"
+            "{category}\n"
+            "{description}\n"
+            "{price}").format(
         description=entry.description.replace("\n", " "),
         mensa=entry.mensa,
         category=entry.category,
-        date_valid=entry.date_valid.strftime("%A, %d.%m.%Y")
+        date_valid=entry.date_valid.strftime("%A, %d.%m.%Y"),
+        price=currency(entry.price/100)
     )
+
+
+if __name__ == "__main__":
+    test_email_generation(datetime.date(2017, 1, 12))
