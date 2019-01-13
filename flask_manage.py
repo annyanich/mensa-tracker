3c46aeabd94a544e1fdfdce009bcf92dde009183
Ann Yanich <ann.yanich@posteo.de>
2019-01-13 06:06:11 +0100

    Modify flask_manage.py to enable ssl in dev env

--- flask_manage.py
+++ flask_manage.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
 from flask_app import app, db
 
-from flask_script import Manager
+from flask_script import Manager, Server
 
 from flask_migrate import Migrate, MigrateCommand
+
 migrate = Migrate(app, db)
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
+
+# Use a fake bad ssl certificate if running on developer's PC
+# (do not deploy the website this way.)
+manager.add_command("runserverdev", Server(ssl_context="adhoc"
+                                           # ssl_crt="cert.pem",
+                                           # ssl_key="key.pem"
+                                           ))
 
 if __name__ == '__main__':
     manager.run()
