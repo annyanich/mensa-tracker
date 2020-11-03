dc5ae4d9d08fd32ce494175930a054fb51ed21a6
Ann Yanich <ann.yanich@posteo.de>
2020-11-03 12:53:18 +0100

    Add 'python-dotenv' to import .env automatically in config.py

--- config.py
+++ config.py
@@ -5,6 +5,9 @@
 import os
 import locale
 
+from dotenv import load_dotenv
+load_dotenv(verbose=True)
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 locale.setlocale(locale.LC_MONETARY, 'de_DE.utf8')
@@ -95,24 +98,26 @@ menu_urls_and_names = {
     # "https://www.studentenwerk-oldenburg.de/de/gastronomie/speiseplaene"
     # "/uhlhornsweg-culinarium.html": "Uhlhornsweg Culinarium",
     # "https://www.studentenwerk-oldenburg.de/de/gastronomie/speiseplaene"
-    # "/uhlhornsweg-ausgabe-a.html": "Uhlhornsweg Pasta & Veggie/Vegan"
+    # "/uhlhornsweg-ausgabe-a.html": "Uhlhornsweg Pasta & Veggie/Vegan",
 
     # "file:///vagrant/test%20menu%20pages/uhlhornsweg-culinarium-20161222.html":
     #     "Uhlhornsweg Culinarium",
-    "file:///vagrant/test%20menu%20pages/uhlhornsweg-culinarium-20170106.html":
-        "Uhlhornsweg Culinarium",
     # "file:///vagrant/test%20menu%20pages/uhlhornsweg-pastaveggievegan-20161222.html":
     #     "Uhlhornsweg Pasta & Veggie/Vegan",
-    "file:///vagrant/test%20menu%20pages/uhlhornsweg-pastaveggievegan-20170106.html":
-        "Uhlhornsweg Pasta & Veggie/Vegan",
     # "file:///vagrant/test%20menu%20pages/uhlhornsweg-classic-20161222.html":
     #     "Uhlhornsweg Classic",
+    # "file:///vagrant/test%20menu%20pages/wechloy-20161222.html": "Wechloy",
+    # "file:///vagrant/test%20menu%20pages/ofener-strasse-20161223.html": "Ofener Straße",
+
+    "file:///vagrant/test%20menu%20pages/uhlhornsweg-culinarium-20170106.html":
+        "Uhlhornsweg Culinarium",
+    "file:///vagrant/test%20menu%20pages/uhlhornsweg-pastaveggievegan-20170106.html":
+        "Uhlhornsweg Pasta & Veggie/Vegan",
     "file:///vagrant/test%20menu%20pages/uhlhornsweg-classic-20170106.html":
         "Uhlhornsweg Classic",
-    # "file:///vagrant/test%20menu%20pages/wechloy-20161222.html": "Wechloy",
-    "file:///vagrant/test%20menu%20pages/wechloy-20170106.html": "Wechloy",
-    # "file:///vagrant/test%20menu%20pages/ofener-strasse-20161223.html": "Ofener Straße",
-    "file:///vagrant/test%20menu%20pages/ofener-strasse-20170106.html": "Ofener Straße",
+    # "file:///vagrant/test%20menu%20pages/wechloy-20170106.html": "Wechloy",
+    # "file:///vagrant/test%20menu%20pages/ofener-strasse-20170106.html": "Ofener Straße",
+
     # "file:///vagrant/test%20menu%20pages/emden-20161223.html": "Emden",
     # "file:///vagrant/test%20menu%20pages/wilhelmshaven-20161223.html": "Wilhelmshaven",
     # "file:///vagrant/test%20menu%20pages/elsfleth-20161223.html": "Elsfleth"
